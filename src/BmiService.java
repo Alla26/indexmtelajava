ca0720c751d122d1823ae317af8fe9c03be6be2a
unknown <girlalla@yandex.ru>
2022-06-24 13:21:42 +0300

    Закомментировала лишнее в Main

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate(int weight, int height) {
         int result;
-        result = weight * 10_000 / (height * height);
+        result = (weight * 10_000 / (height * height));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 58;
-        int height = 170;
-        int index = service.calculate(58, 170);
+        // int weight = 58;
+        // int height = 180;
+        int index = service.calculate(45, 180);
         System.out.println(("Индекс массы тела:   ") + (index));
     }
 }
